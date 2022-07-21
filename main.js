7d5dc04f463f02807d99f002b6ed0f68c2624c47
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-07-21 08:57:42 +0200

    add ciclo per stampa post

--- main.js
+++ main.js
@@ -55,3 +55,64 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+let postContainer = document.getElementById("container");
+
+
+
+for(let i=0; i<posts.length; i++){
+    console.log(posts[i].author.name);
+
+
+    postContainer.innerHTML +=   `
+    <div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${posts[i].author.image}" alt="${posts[i].author.name}">                    
+            </div>
+            <div class="post-meta__data">
+            <div id="post-meta__author"><h3>${posts[i].author.name}</h3></div>
+            <div class="post-meta__time">${posts[i].created}</div>
+        </div>                    
+    </div>
+</div>
+<div class="post__text">${posts[i].content}</div>
+<div class="post__image">
+    <img src="${posts[i].media}" alt="">
+</div>
+<div class="post__footer">
+    <div class="likes js-likes">
+        <div class="likes__cta">
+            <a id="button-like" class="like-button  js-like-button" href="#" data-postid="1">
+                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                <span class="like-button__label">Mi Piace</span>
+            </a>
+        </div>
+        <div class="likes__counter">
+            Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+        </div>
+    </div> 
+</div>
+</div> 
+`;
+
+
+if(posts[i].author.image == null){
+    let imageFallback = document.querySelector(".profile-pic");
+    let div = document.createElement("div")
+
+    imageFallback.append(div);
+
+};
+
+};
+
+let button = document.getElementById("button-like");
+
+
+button.addEventListener("click",
+    function(){
+        button.classList.add("greencolor");
+    }
+    )
